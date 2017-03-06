5bf0661a20ad6bd996552283bc3d7b3a09c2dfcc
vaibhao90 <vaibhao.dumore75@gmail.com>
2017-03-07 00:24:49 +0530

    change www

--- www/js/app.js
+++ www/js/app.js
@@ -26,7 +26,8 @@ angular.module('starter', ['ionic','starter.controllers'])
     $stateProvider
         .state('map', {
             url: '/map',
-            templateUrl: 'templates/map.html',
+            //templateUrl: 'templates/map.html',
+            templateUrl: 'www/templates/map.html',
             controller: 'MapCtrl'
         })
 
